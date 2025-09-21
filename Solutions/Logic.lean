b35cf2772e3d9c954199b038f28eb0c8123b4926
Gabriel-Soares-Gomes <gabrielsoaresgomes14@gmail.com>
2025-09-21 19:09:06 -0300

    aLotOfThingsSorry

---
+++ Solutions/Logic.lean
@@ -0,0 +1,465 @@
+section propositional
+
+variable (P Q R : Prop)
+
+
+------------------------------------------------
+-- Double negation
+------------------------------------------------
+
+theorem doubleneg_intro :
+  P → ¬ ¬ P  := by
+  intro hp
+  intro hnp
+  apply hnp hp
+
+theorem doubleneg_elim :
+  ¬ ¬ P → P  := by
+  intro notnotP
+  by_cases lem : P
+  case pos =>
+    exact lem
+  case neg =>
+    contradiction
+
+theorem doubleneg_law :
+  ¬ ¬ P ↔ P  := by
+  constructor
+  case mp =>
+    intro notnotP
+    by_cases lem : P
+    case pos =>
+      exact lem
+    case neg =>
+      contradiction
+
+  case mpr =>
+    intro hp
+    intro notp
+    contradiction
+
+
+
+------------------------------------------------
+-- Commutativity of ∨,∧
+------------------------------------------------
+
+theorem disj_comm :
+  (P ∨ Q) → (Q ∨ P)  := by
+  intro PorQ
+  rcases PorQ with ( h1 | h2)
+  case inl =>
+    right
+    exact h1
+
+  case inr =>
+    left
+    exact h2
+
+
+theorem conj_comm :
+  (P ∧ Q) → (Q ∧ P)  := by
+  intro PeQ
+  rcases PeQ with ⟨h1, h2⟩
+  constructor
+  case intro.left =>
+    exact h2
+  case intro.right =>
+    exact h1
+
+
+------------------------------------------------
+-- Interdefinability of →,∨
+------------------------------------------------
+
+theorem impl_as_disj_converse :
+  (¬ P ∨ Q) → (P → Q)  := by
+  intro notPorQ
+  rcases notPorQ with (h1 | h2)
+  case inl =>
+    intro hp
+    contradiction
+  case inr =>
+    intro hp
+    exact h2
+
+
+theorem disj_as_impl :
+  (P ∨ Q) → (¬ P → Q)  := by
+  intro PeQ
+  rcases PeQ with (h1 | h2)
+  case inl =>
+    intro notP
+    contradiction
+  case inr =>
+    intro notP
+    exact h2
+
+
+------------------------------------------------
+-- Contrapositive
+------------------------------------------------
+
+theorem impl_as_contrapositive :
+  (P → Q) → (¬ Q → ¬ P)  := by
+  intro PimpQ
+  intro notQ
+  intro hp
+  have hq := PimpQ hp
+  apply notQ hq
+
+theorem impl_as_contrapositive_converse :
+  (¬ Q → ¬ P) → (P → Q)  := by
+  intro naoQimpnaoP
+  intro p
+  by_cases lem : Q
+  case pos =>
+    exact lem
+  case neg =>
+    have notP := naoQimpnaoP lem
+    contradiction
+
+theorem contrapositive_law :
+  (P → Q) ↔ (¬ Q → ¬ P)  := by
+  constructor
+  case mp =>
+    intro PimpQ
+    intro notQ
+    intro p
+    have q := PimpQ p
+    contradiction
+
+  case mpr =>
+    intro NotQimpNotP
+    intro p
+    by_cases lem : Q
+    case pos =>
+      exact lem
+    case neg =>
+      have notP := NotQimpNotP lem
+      contradiction
+
+
+------------------------------------------------
+-- Irrefutability of LEM[P]
+------------------------------------------------
+
+theorem lem_irrefutable :
+  ¬ ¬ (P ∨ ¬ P)  := by
+  intro naoPORQ
+  apply naoPORQ
+  by_cases lem : P
+  case pos =>
+    left
+    exact lem
+  case neg =>
+    right
+    exact lem
+
+
+
+------------------------------------------------
+-- Peirce's law
+------------------------------------------------
+
+theorem peirce_law_weak :
+  ((P → Q) → P) → ¬ ¬ P  := by
+  intro hpqp
+  intro notP
+  by_cases lem : P
+  case pos =>
+    contradiction
+  case neg =>
+    apply notP
+    apply hpqp
+    intro p
+    contradiction
+
+
+------------------------------------------------
+-- Linearity of →
+------------------------------------------------
+
+theorem impl_linear :
+  (P → Q) ∨ (Q → P)  := by
+  sorry
+
+
+------------------------------------------------
+-- Interdefinability of ∨,∧
+------------------------------------------------
+
+theorem disj_as_negconj :
+  P ∨ Q → ¬ (¬ P ∧ ¬ Q)  := by
+  sorry
+
+theorem conj_as_negdisj :
+  P ∧ Q → ¬ (¬ P ∨ ¬ Q)  := by
+  sorry
+
+
+------------------------------------------------
+-- De Morgan laws for ∨,∧
+------------------------------------------------
+
+theorem demorgan_disj :
+  ¬ (P ∨ Q) → (¬ P ∧ ¬ Q)  := by
+  sorry
+
+theorem demorgan_disj_converse :
+  (¬ P ∧ ¬ Q) → ¬ (P ∨ Q)  := by
+  sorry
+
+theorem demorgan_conj :
+  ¬ (P ∧ Q) → (¬ Q ∨ ¬ P)  := by
+  sorry
+
+theorem demorgan_conj_converse :
+  (¬ Q ∨ ¬ P) → ¬ (P ∧ Q)  := by
+  sorry
+
+theorem demorgan_conj_law :
+  ¬ (P ∧ Q) ↔ (¬ Q ∨ ¬ P)  := by
+  sorry
+
+theorem demorgan_disj_law :
+  ¬ (P ∨ Q) ↔ (¬ P ∧ ¬ Q)  := by
+  sorry
+
+
+------------------------------------------------
+-- Distributivity laws between ∨,∧
+------------------------------------------------
+
+theorem distr_conj_disj :
+  P ∧ (Q ∨ R) → (P ∧ Q) ∨ (P ∧ R)  := by
+  sorry
+
+theorem distr_conj_disj_converse :
+  (P ∧ Q) ∨ (P ∧ R) → P ∧ (Q ∨ R)  := by
+  sorry
+
+theorem distr_disj_conj :
+  P ∨ (Q ∧ R) → (P ∨ Q) ∧ (P ∨ R)  := by
+  sorry
+
+theorem distr_disj_conj_converse :
+  (P ∨ Q) ∧ (P ∨ R) → P ∨ (Q ∧ R)  := by
+  sorry
+
+
+------------------------------------------------
+-- Currying
+------------------------------------------------
+
+theorem curry_prop :
+  ((P ∧ Q) → R) → (P → (Q → R))  := by
+  sorry
+
+theorem uncurry_prop :
+  (P → (Q → R)) → ((P ∧ Q) → R)  := by
+  sorry
+
+
+------------------------------------------------
+-- Reflexivity of →
+------------------------------------------------
+
+theorem impl_refl :
+  P → P  := by
+  sorry
+
+
+------------------------------------------------
+-- Weakening and contraction
+------------------------------------------------
+
+theorem weaken_disj_right :
+  P → (P ∨ Q)  := by
+  sorry
+
+theorem weaken_disj_left :
+  Q → (P ∨ Q)  := by
+  sorry
+
+theorem weaken_conj_right :
+  (P ∧ Q) → P  := by
+  sorry
+
+theorem weaken_conj_left :
+  (P ∧ Q) → Q  := by
+  sorry
+
+
+------------------------------------------------
+-- Idempotence of ∨,∧
+------------------------------------------------
+
+theorem disj_idem :
+  (P ∨ P) ↔ P  := by
+  sorry
+
+theorem conj_idem :
+  (P ∧ P) ↔ P := by
+  sorry
+
+
+------------------------------------------------
+-- Bottom, Top
+------------------------------------------------
+
+theorem false_bottom :
+  False → P := by
+  sorry
+
+theorem true_top :
+  P → True  := by
+  sorry
+
+
+end propositional
+
+----------------------------------------------------------------
+
+section predicate
+
+variable (U : Type)
+variable (P Q : U → Type)
+
+
+------------------------------------------------
+-- De Morgan laws for ∃,∀
+------------------------------------------------
+
+theorem demorgan_exists :
+  ¬ (∃ x, P x) → (∀ x, ¬ P x)  := by
+  sorry
+
+theorem demorgan_exists_converse :
+  (∀ x, ¬ P x) → ¬ (∃ x, P x)  := by
+  sorry
+
+theorem demorgan_forall :
+  ¬ (∀ x, P x) → (∃ x, ¬ P x)  := by
+  sorry
+
+theorem demorgan_forall_converse :
+  (∃ x, ¬ P x) → ¬ (∀ x, P x)  := by
+  sorry
+
+theorem demorgan_forall_law :
+  ¬ (∀ x, P x) ↔ (∃ x, ¬ P x)  := by
+  sorry
+
+theorem demorgan_exists_law :
+  ¬ (∃ x, P x) ↔ (∀ x, ¬ P x)  := by
+  sorry
+
+
+------------------------------------------------
+-- Interdefinability of ∃,∀
+------------------------------------------------
+
+theorem exists_as_neg_forall :
+  (∃ x, P x) → ¬ (∀ x, ¬ P x)  := by
+  sorry
+
+theorem forall_as_neg_exists :
+  (∀ x, P x) → ¬ (∃ x, ¬ P x)  := by
+  sorry
+
+theorem forall_as_neg_exists_converse :
+  ¬ (∃ x, ¬ P x) → (∀ x, P x)  := by
+  sorry
+
+theorem exists_as_neg_forall_converse :
+  ¬ (∀ x, ¬ P x) → (∃ x, P x)  := by
+  sorry
+
+theorem forall_as_neg_exists_law :
+  (∀ x, P x) ↔ ¬ (∃ x, ¬ P x)  := by
+  sorry
+
+theorem exists_as_neg_forall_law :
+  (∃ x, P x) ↔ ¬ (∀ x, ¬ P x)  := by
+  sorry
+
+
+------------------------------------------------
+--  Distributivity between quantifiers
+------------------------------------------------
+
+theorem exists_conj_as_conj_exists :
+  (∃ x, P x ∧ Q x) → (∃ x, P x) ∧ (∃ x, Q x)  := by
+  sorry
+
+theorem exists_disj_as_disj_exists :
+  (∃ x, P x ∨ Q x) → (∃ x, P x) ∨ (∃ x, Q x)  := by
+  sorry
+
+theorem exists_disj_as_disj_exists_converse :
+  (∃ x, P x) ∨ (∃ x, Q x) → (∃ x, P x ∨ Q x)  := by
+  sorry
+
+theorem forall_conj_as_conj_forall :
+  (∀ x, P x ∧ Q x) → (∀ x, P x) ∧ (∀ x, Q x)  := by
+  sorry
+
+theorem forall_conj_as_conj_forall_converse :
+  (∀ x, P x) ∧ (∀ x, Q x) → (∀ x, P x ∧ Q x)  := by
+  sorry
+
+theorem forall_disj_as_disj_forall_converse :
+  (∀ x, P x) ∨ (∀ x, Q x) → (∀ x, P x ∨ Q x)  := by
+  sorry
+
+
+end predicate
+
+------------------------------------------------
+
+section bonus
+
+------------------------------------------------
+--  Drinker's paradox
+------------------------------------------------
+
+variable (D : U → Prop)
+
+-- There is a person p such that:
+-- if p drinks, then everybody drinks
+-- p: «person»
+-- D x: «x drinks»
+theorem drinker :
+  ∃ p, (D p → ∀ x, D x)  := by
+  sorry
+
+------------------------------------------------
+--  Russell's paradox
+------------------------------------------------
+
+variable (S : U → U → Prop)
+
+-- It is impossible to have a barber b such that
+-- b shaves exactly those people who do not shave themselves
+-- b: «barber»
+-- S x y: «x shaves y»
+theorem russell :
+  ¬ ∃ b, ∀ x, (S b x ↔ ¬ S x x)  := by
+  sorry
+
+
+end bonus
+
+
+/- NOT THEOREMS --------------------------------
+
+theorem forall_disj_as_disj_forall :
+  (∀ x, P x ∨ Q x) → (∀ x, P x) ∨ (∀ x, Q x)  := by
+  sorry
+
+theorem exists_conj_as_conj_exists_converse :
+  (∃ x, P x) ∧ (∃ x, Q x) → (∃ x, P x ∧ Q x)  := by
+  sorry
+
+---------------------------------------------- -/
